4e1caa67c2b9d2078b4ce0f4e8d6ffc1113a27ce
taxio <taxio.2029@gmail.com>
2018-11-03 17:52:37 +0900

    routing

--- mizql/frontend/ui/src/App.js
+++ mizql/frontend/ui/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path='/' component={Home}/>
+      <Route path='/login' component={Login}/>
+    </div>
+  </BrowserRouter>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Not Implemented</p>
+  </div>
+);
+
+const Login = () => (
+  <div>
+    <h2>Login</h2>
+    <p>Not Implemented</p>
+  </div>
+);
 
 export default App;
