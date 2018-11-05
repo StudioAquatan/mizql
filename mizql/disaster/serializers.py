aed54597c841dbfc0a02c0debb9a1a66cc6eff64
Pudding <s.kokuryo@gmail.com>
2018-11-05 23:34:58 +0900

    implement serializer for location and alarm

---
+++ mizql/disaster/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Location, Alarm, DemoLocation, DemoAlarm
+
+
+class AlarmSerializer(serializers.ModelSerializer):
+    """
+    注意報・警報のシリアライザ
+    """
+
+    class Meta:
+        model = Alarm
+        fields = ('pk', 'code', 'name', 'created_at', 'updated_at', 'deleted_at')
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    """
+    地域情報のシリアライザ
+    """
+
+    alarms = AlarmSerializer(read_only=True, allow_null=True, many=True)
+
+    class Meta:
+        model = Location
+        fields = ('pk', 'name', 'alarms', 'updated_at')
+
+
+class DemoAlarmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DemoAlarm
+        fields = ('pk', 'code', 'name', 'created_at', 'updated_at', 'deleted_at')
+
+
+class DemoLocationSerializer(serializers.ModelSerializer):
+    alarms = DemoAlarmSerializer(read_only=True, allow_null=True, many=True)
+
+    class Meta:
+        model = DemoLocation
+        fields = ('pk', 'name', 'alarms', 'updated_at')
